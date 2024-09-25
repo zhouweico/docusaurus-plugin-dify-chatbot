cd517a8d5f354954a267ca393d7358894e9b079c
zhouweico <zhouwei.will@gmail.com>
2024-09-25 17:39:13 +0800

    Docusaurus Plugin Dify Chatbot

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-module.exports = function (context, options) {
+export default function difyChatbotPlugin (context, options) {
   const {
     token,
     baseUrl,
-    isDev,
-    draggable,
-    dragAxis,
+    isDev = false,
+    draggable = false,
+    dragAxis = 'both',
     containerProps,
-    inputs,
+    inputs = {},
     bubbleButtonStyle,
   } = options;
 
-  return {
-    name: 'dify-chatbot-plugin',
-    injectHtml: true,
-    injectHtml: (html) => {
-      const embedCode = `
-        <script>
-          window.difyChatbotConfig = {
-            token: '${token}',
-            isDev: ${isDev || false},
-            baseUrl: '${baseUrl || (isDev ? 'https://dev.udify.app' : 'https://udify.app')}',
-            containerProps: ${JSON.stringify(containerProps || {})},
-            draggable: ${draggable || false},
-            dragAxis: '${dragAxis || 'both'}',
-            inputs: ${JSON.stringify(inputs || {})}
-          };
-        </script>
-        <script
-          src="https://udify.app/embed.min.js"
-          id="${token}"
-          defer>
-        </script>
-        <style>
-          #dify-chatbot-bubble-button {
-            ${bubbleButtonStyle || 'background-color: #1C64F2 !important;'} /* 使用配置项 */
-          }
-        </style>
-      `;
+  if (!token) {
+    throw new Error('Token is required for the Dify Chatbot plugin.');
+  }
 
-      return html.replace(
-        '</body>',
-        `${embedCode}</body>`
-      );
+  const embedCode = `
+    <script>
+      window.difyChatbotConfig = {
+        token: '${token}',
+        isDev: ${isDev},
+        baseUrl: '${baseUrl || (isDev ? 'https://dev.udify.app' : 'https://udify.app')}',
+        containerProps: ${JSON.stringify(containerProps || {})},
+        draggable: ${draggable},
+        dragAxis: '${dragAxis}',
+        inputs: ${JSON.stringify(inputs)}
+      };
+    </script>
+    <script
+      src="https://udify.app/embed.min.js"
+      id="${token}"
+      defer>
+    </script>
+    <style>
+      #dify-chatbot-bubble-button {
+        ${bubbleButtonStyle || 'background-color: #1C64F2 !important;'} /* 使用配置项 */
+      }
+    </style>
+  `;
+
+  return {
+    name: 'docusaurus-plugin-dify-chatbot',
+    injectHtmlTags({content}) {
+      return {
+        postBodyTags: [`${embedCode}`],
+      };
     },
   };
 };
